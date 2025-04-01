6510b679b2ca8b26344fc411d4fa24751a4234a2
pratik <pdhakal906@gmail.com>
2025-04-01 21:36:45 +0545

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
+        <link rel="icon" href="/icon.png" sizes="any" />
         <ColorSchemeScript />
       </head>
       <body>
