4337fc598d43a40b1de08e91aad4598fc0edd180
pratik <pdhakal906@gmail.com>
2025-06-14 11:11:06 +0545

    feat: change news page design

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,34 +1,41 @@
 'use client';
-import { Card, Text, Image } from '@mantine/core'
-import React from 'react'
+import { Card, Text, Stack, Group, Box } from '@mantine/core';
+import { IconArrowUp, IconArrowDown } from '@tabler/icons-react';
+import React from 'react';
 import { NewsItemType } from './types';
+
 interface NewsCardProps {
   newsItem: NewsItemType;
 }
 
 const NewsCard = ({ newsItem }: NewsCardProps) => {
+  const date = new Date(newsItem.created_at);
+  const formattedDate = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
   return (
     <Card
       shadow="sm"
-      padding="xl"
+      padding="sm"
       component="a"
       href={newsItem.link}
       target="_blank"
+      withBorder
     >
-      <Card.Section>
-        <Image
-          fallbackSrc='fallback_image.jpeg'
-          src={newsItem.image_url}
-          h={160}
-          alt="Featured Image"
-        />
-      </Card.Section>
-
-      <Text fw={600} size="xl" mt="md">
-        {newsItem.title}
-      </Text>
+      <Group justify='space-between' align="center">
+        <Box>
+          <Text fw={300} size="lg">
+            {newsItem.title}
+          </Text>
+          <Text fw={200} c={'dimmed'} size='sm' >
+            {formattedDate}
+          </Text>
+        </Box>
+      </Group>
     </Card>
   );
-}
+};
 
-export default NewsCard
+export default NewsCard;
--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -3,24 +3,13 @@ import NewsCard from './NewsCard'
 import { SimpleGrid, Stack } from '@mantine/core';
 import { NewsPropType } from './types';
 
-const NewsGrid = (props: NewsPropType) => {
-  const { newsItem } = props
+const NewsGrid = ({ newsItem }: NewsPropType) => {
   return (
-    <Stack
-      p={10}
-      gap={20}>
-      <SimpleGrid
-        cols={{ base: 1, sm: 2, lg: 4 }}
-        spacing={{ base: 10, sm: 'xl' }}
-        verticalSpacing={{ base: 'md', sm: 'xl' }}
-      >
-        {newsItem.map((news) => (
-          <NewsCard key={news.id} newsItem={news} />
-        ))}
-      </SimpleGrid>
-
+    <Stack gap="md">
+      {newsItem.map((news) => (
+        <NewsCard key={news.id} newsItem={news} />
+      ))}
     </Stack>
   );
-}
-
+};
 export default NewsGrid
